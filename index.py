f874e77e935ac5a5831e1146a0d1b8cda7c64e33
lukix <lukix.95@gmail.com>
2018-12-28 16:42:01 +0100

    Add time acceleration

--- index.py
+++ index.py
@@ -15,6 +15,7 @@ SKY_COLOR = (0, 0, 0)
 shouldCloseApp = False
 framesPerSecond = 60
 frameTime = 1.0 / framesPerSecond
+timeSpeedMultiplier = 1.0
 
 while not shouldCloseApp:
 	screen.fill(SKY_COLOR)
@@ -28,6 +29,14 @@ while not shouldCloseApp:
 				shouldCloseApp = True
 			if event.key == pygame.K_SPACE:
 				spaceship.unfroze()
+			if event.key == pygame.K_1:
+				timeSpeedMultiplier = 1
+			if event.key == pygame.K_2:
+				timeSpeedMultiplier = 2
+			if event.key == pygame.K_3:
+				timeSpeedMultiplier = 4
+			if event.key == pygame.K_4:
+				timeSpeedMultiplier = 8
 
 	if pygame.key.get_pressed()[pygame.K_LEFT]:
 		spaceship.setAntiClockwiseRotation()
@@ -43,12 +52,12 @@ while not shouldCloseApp:
 	pygame.display.flip()
 
 	altitude = earth.position.distance_to(spaceship.position) - (2 * 256)
-	airDensity = 8e-3 * (1 - 2.5e-3 * altitude) ** 8
+	airDensity = 6e-3 * (1 - 2.5e-3 * altitude) ** 8
 
 	spaceship.applyGravityTowards(1e8, earth.position) # Gravity
 
 	spaceship.applyDrag(0 if altitude > 250 else airDensity)
 
-	spaceship.update(frameTime)
+	spaceship.update(frameTime * timeSpeedMultiplier)
 
 	clock.tick(framesPerSecond)
