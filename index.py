ce16ac82f0914cab1306535cf98782b842492520
lukix <lukix.95@gmail.com>
2018-12-28 17:12:17 +0100

    Add crash detection and game restarting

--- index.py
+++ index.py
@@ -8,11 +8,17 @@ pygame.display.set_caption('Ad Astra')
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
+mainFont = pygame.font.SysFont("arial", 40)
+mainFont.set_bold(True)
+
+normalFont = pygame.font.SysFont("arial", 20)
+
 spaceship = Spaceship()
 earth = Planet()
 SKY_COLOR = (0, 0, 0)
 
 shouldCloseApp = False
+isGameOver = False
 framesPerSecond = 60
 frameTime = 1.0 / framesPerSecond
 timeSpeedMultiplier = 1.0
@@ -28,7 +34,12 @@ while not shouldCloseApp:
 			if event.key == pygame.K_ESCAPE:
 				shouldCloseApp = True
 			if event.key == pygame.K_SPACE:
-				spaceship.unfroze()
+				if isGameOver:
+					spaceship = Spaceship()
+					isGameOver = False
+					timeSpeedMultiplier = 1
+				else:
+					spaceship.unfroze()
 			if event.key == pygame.K_1:
 				timeSpeedMultiplier = 1
 			if event.key == pygame.K_2:
@@ -49,8 +60,6 @@ while not shouldCloseApp:
 	earth.render(screen, spaceship.position)
 	spaceship.render(screen, spaceship.position)
 
-	pygame.display.flip()
-
 	altitude = earth.position.distance_to(spaceship.position) - (2 * 256)
 	airDensity = 6e-3 * (1 - 2.5e-3 * altitude) ** 8
 
@@ -60,4 +69,17 @@ while not shouldCloseApp:
 
 	spaceship.update(frameTime * timeSpeedMultiplier)
 
+	if spaceship.hasCrashed(earth.position, 2 * 256):
+		isGameOver = True
+		timeSpeedMultiplier = 0
+
+	if isGameOver:
+		label1 = mainFont.render("Game Over!", 1, (255,255,0))
+		label2 = normalFont.render("Press \"space\" to try again.", 1, (255,255,0))
+		label3 = normalFont.render("Press \"escape\" to quit.", 1, (255,255,0))
+		screen.blit(label1, (50, 50))
+		screen.blit(label2, (55, 100))
+		screen.blit(label3, (55, 130))
+
+	pygame.display.flip()
 	clock.tick(framesPerSecond)
